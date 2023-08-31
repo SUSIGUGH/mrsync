8f773f247ff7c2c6e8fac075c1bd9ff4a8128c1c
SuSi GuGh <86735047+SUSIGUGH@users.noreply.github.com>
2023-08-31 14:40:52 +0530

    Update mirror-sync.sh

--- .github/workflows/mirror-sync.sh
+++ .github/workflows/mirror-sync.sh
@@ -1,6 +1,6 @@
 rm -Rf mirror01.git
 git clone --mirror https://susigugh01+1-at-016589193088:XcNhCrkDe+dPndDYWURRJUCzMOxOdi0SBoOHpuSbOsc=@git-codecommit.us-east-1.amazonaws.com/v1/repos/mirror01
 cd mirror01.git
-git remote set-url --push origin https://${{ secrets.PAT }}@github.com/SUSIGUGH/mirror01.git
+git remote set-url --push origin https://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@github.com/SUSIGUGH/mirror01.git
 git fetch -p origin
 git push --mirror
